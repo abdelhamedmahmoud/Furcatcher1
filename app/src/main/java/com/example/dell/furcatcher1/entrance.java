758c84107cc2ced760cf6e93bb8871ff5a23f9c6
abdelhamedmahmoud <abdo42013048@gmail.com>
2016-11-22 12:04:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dell/furcatcher1/entrance.java
@@ -0,0 +1,13 @@
+package com.example.dell.furcatcher1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class entrance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrance);
+    }
+}
