99d93d1e8383222aa18c203ce4195b93e88e506b
KareemGamal <kareemgamal_androidProjects@hotmail.com>
2016-11-23 20:31:07 +0200

    Splash Activity

--- app/src/main/java/com/example/dell/furcatcher1/splash.java
+++ app/src/main/java/com/example/dell/furcatcher1/splash.java
@@ -1,7 +1,12 @@
 package com.example.dell.furcatcher1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
 
 public class splash extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+        Thread Timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                }catch(InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(splash.this, entrance.class);
+                    startActivity(i);
+                }
+            }
+        };
+        Timer.start();
     }
 }
